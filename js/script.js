66edf2c53822131cc2686fe5aaffd03f56149696
大橋祥博 <webcreator0084@gmail.com>
2016-05-22 22:34:24 +0900

    各種変更

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(function () {
-	var width = 800;
-	var height = 300;
-	var default_top = 50 + '%';
-	var default_left = 50 + '%';
-	var top = -150 + 'px';
-	var left = -400 + 'px';
-
+	
 	function mordal() {
+		var width = 800;
+		var height = 300;
+		var default_top = 50 + '%';
+		var default_left = 50 + '%';
+		var top = -150 + 'px';
+		var left = -400 + 'px';
 
 		$('.mordal').animate({
 			"width": width,
@@ -16,13 +16,13 @@ $(function () {
 		}, 700).show();
 		$('.bg3').css('display', 'block');
 		$('.bg3').click(function () {
-			$(this).fadeOut();
-			$('.mordal').fadeOut().css({
+			$(this).fadeOut(300);
+			$('.mordal').css({
 				"width": 0,
 				"height": 0,
-				"top": default_top,
-				"left": default_left
-			});
+				"margin-top": 0,
+				"margin-left": 0
+			}).fadeOut(300);
 		});
 	};
 
@@ -34,14 +34,14 @@ $(function () {
 			var enemy = $('#output').attr('src');
 			var self = this;
 			var result_img = $(self).attr('data-img');
-
-			$('.mordal').fadeOut().css({
+			
+			$('.mordal').css({
 				"width": 0,
 				"height": 0,
-				"top": default_top,
-				"left": default_left
-			});
-			$('.bg3').fadeOut();
+				"margin-top": 0,
+				"margin-left": 0
+			}).fadeOut(300);
+			$('.bg3').fadeOut(300);
 
 			function win_or_losing(result_img, enemy) {
 				var janken_ob = function() {};
@@ -54,31 +54,31 @@ $(function () {
 				var losing_paturn = new janken_ob();
 				console.log(enemy);
 				console.log(result_img);
-				var paturn1 = janken_ob.goo && janken_ob.tyoki;
-				var paturn2 = janken_ob.tyoki && janken_ob.per;
-				var paturn3 = janken_ob.per && janken_ob.goo;
-				var paturn4 = janken_ob.tyoki && janken_ob.goo;
-				var paturn5 = janken_ob.per && janken_ob.tyoki;
-				var paturn6 = janken_ob.goo && janken_ob.per;
-				
-				if (result_img && enemy === paturn1) {
-					window.alert('あなたの勝ちです');
-				}
-				if (result_img && enemy === paturn2) {
-					window.alert('あなたの勝ちです');
-				}
-				if (result_img && enemy === paturn3) {
-					window.alert('あなたの勝ちです');
-				}
-				if (result_img && enemy === paturn4) {
-					window.alert('あなたの負けです');
-				}
-				if (result_img && enemy === paturn5) {
-					window.alert('あなたの負けです');
-				}
-				if (result_img && enemy === paturn6) {
-					window.alert('あなたの負けです');
-				}
+//				var paturn1 = janken_ob.goo && janken_ob.tyoki;
+//				var paturn2 = janken_ob.tyoki && janken_ob.per;
+//				var paturn3 = janken_ob.per && janken_ob.goo;
+//				var paturn4 = janken_ob.tyoki && janken_ob.goo;
+//				var paturn5 = janken_ob.per && janken_ob.tyoki;
+//				var paturn6 = janken_ob.goo && janken_ob.per;
+//				
+//				if (result_img && enemy === paturn1) {
+//					return 'あなたの勝ちです'
+//				}
+//				if (result_img && enemy === paturn2) {
+//					return 'あなたの勝ちです'
+//				}
+//				if (result_img && enemy === paturn3) {
+//					return 'あなたの勝ちです'
+//				}
+//				if (result_img && enemy === paturn4) {
+//					return 'あなたの負けです'
+//				}
+//				if (result_img && enemy === paturn5) {
+//					return 'あなたの負けです'
+//				}
+//				if (result_img && enemy === paturn6) {
+//					return 'あなたの負けです'
+//				}
 			};
 			win_or_losing(enemy, result_img);
 		});
